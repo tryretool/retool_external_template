c1655852c0d97c15144d31572d7c6eb45887518d
Chris Gray <chris@tyr.vin>
2023-12-07 14:04:39 +0000

    Add config.js

---
+++ frontend/config.js
@@ -0,0 +1,31 @@
+exports.deployOnLocalhost = true;
+
+exports.homepage = {
+  sidebarList: [
+    {
+      title: "Claims Portal",
+      retoolAppName: "claimsPortal",
+      icon: "home",
+      slug: "",
+      groups: ["bronze"],
+    },
+  ],
+};
+exports.theme = {
+  palette: {
+    primary: {
+      main: "#ffffff",
+      light: "#E06363",
+      dark: "#972A2A",
+      contrastText: "#ffffff",
+    },
+  },
+  typography: {
+    fontFamily: "Roboto",
+  },
+};
+exports.formattingPreferences = {
+  backgroundColor: "#ffffff",
+  text: "#080928",
+  elevation: 1,
+};
