84c64f511c6c3d2ad24d3dfda9a90b64b7a94891
Chris Gray <chris@tyr.vin>
2023-12-07 12:44:26 +0000

    Fix build

--- infrastructure/build_scripts/build.sh
+++ infrastructure/build_scripts/build.sh
@@ -4,14 +4,17 @@ export IMAGE_TAG=$(echo "$CODEBUILD_RESOLVED_SOURCE_VERSION" | head -c 7)
 export LATEST_TAG="latest"
 echo IMAGE_TAG="$IMAGE_TAG"
 echo LATEST_TAG="$LATEST_TAG"
+
+npm i
+
 if [[ $BUILD = "true" ]]; then
-	echo Building Docker container
-	docker build \
-		--no-cache \
-		-t "$REPOSITORY_URI:$IMAGE_TAG" \
-		-t "$REPOSITORY_URI:$LATEST_TAG" \
-		.
-	echo Build completed on "$(date)"
+    echo Building Docker container
+    docker build \
+        --no-cache \
+        -t "$REPOSITORY_URI:$IMAGE_TAG" \
+        -t "$REPOSITORY_URI:$LATEST_TAG" \
+        .
+    echo Build completed on "$(date)"
 else
-	echo Skipping build — \$BUILD is disabled.
+    echo Skipping build — \$BUILD is disabled.
 fi
